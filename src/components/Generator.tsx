40337265a895afd710b81302ff1163ca9ec20a7f
skykiah <likai1993118@gmail.com>
2023-03-11 10:43:19 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -224,7 +224,7 @@ export default () => {
             class='gen-textarea'
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            send
+            Send
           </button>
           <button title="Clear" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
